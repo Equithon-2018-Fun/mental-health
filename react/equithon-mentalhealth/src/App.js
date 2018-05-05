0baafb39307c4122b9dda1669ba9a5e3b37267ff
Eric Chan <hazzelnutst@gmail.com>
2018-05-05 14:44:14 -0400

    NEW: equithon-mentalhealth2 folder created & post working

--- react/equithon-mentalhealth/src/App.js
+++ react/equithon-mentalhealth/src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import _ from 'lodash';
+import { db } from './firebase';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+    };
+
+    _.bindAll(this, 'onChange');
+  }
+
+  post() {
+    console.log('hello');
+    db.collection('posts').add({
+      message: this.state.message,    
+    }).then(ref => {
+      console.log('Added document with ID: ', ref.id);
+    });
+  }
+
+  onChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      message: event.target.value,
+    });
+  }
+
   render() {
+    let { message } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +43,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <input value={message} onChange={this.onChange} />
+        <button onClick={this.post}>post</button>
+
       </div>
     );
   }
