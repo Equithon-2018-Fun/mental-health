2ffa51a82db951c8bc17595ca9ed8e6bac27512b
tkfi <nick.yang3721@gmail.com>
2018-05-05 11:33:11 -0400

    Revert "Update for Files"
    This reverts commit 3fee9ccd4ce0c987d19f38dea5bac1592ee3c8d2.

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\nicky\Documents\GitHub\mental-health"
+set "VIRTUAL_ENV=C:\Users\nicky\mental-health"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
