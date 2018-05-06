78af6fe8f4714601399aa99ffff269db949dbdb2
Eric Chan <hazzelnutst@gmail.com>
2018-05-05 20:36:40 -0400

    Convert home html to React

--- react/myapp/src/containers/dashboard/Dashboard.jsx
+++ react/myapp/src/containers/dashboard/Dashboard.jsx
@@ -6,36 +6,63 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div className="full">
-        <div className="header"></div>
-        <div className="welcome" className="contents">
-          <h1 className="title-font">Hello! </h1>
+      <div class="full">
+        <div class="header"></div>
+        <div class="welcome">
+          <h1 class="title-font">Hello! </h1>
         </div>
-        <div className="input-info" className="contents">
-          <h3 className="title-font">How was your day?</h3>
+        <div class="input-info">
+          <h3 class="title-font">How was your day?</h3>
           <form>
             <span>Input an achievement: </span>
             <br/><br/>
-            <textarea name="comment" form="usrform" placeholder="Something positive, like winning a competition or an awesome vacation!" cols={3} rows={3}>
-            </textarea>
-            <br/><br/>
-          <h3 className="title-font">How's your mood compared to last time?</h3>
-            <button type="button" value=""> Not So Good</button>
-            <button type="button" value=""> About the Same</button>
-            <button type="button" value=""> Better!</button>
+            <textarea name="comment" form="usrform" placeholder="Something positive, like winning a competition or an awesome vacation!" cols={3} rows={3}></textarea>
             <br/>
-            <br/>
+            <h3 class="title-font">How is your mood compared to last time you checked in?</h3>
+            <div class="message"><span class="message-text" /></div>
+            <div class="btn-group">
+              <label class="btn active">
+              <input type="radio" name="options" id="option1" value="1" />
+              <label for="option1">
+                <span class="btn">Worse...</span>
+              </label>
+              </label>
+              <label class="btn">
+              <input type="radio" name="options" id="option2" value="2" />
+              <label for="option2">
+                <span class="btn">Meh...</span>
+              </label>
+              </label>
+              <label class="btn">
+              <input type="radio" name="options" id="option3" value="3" />
+                <label for="option3">
+                  <span class="btn">The same</span>
+                </label>
+              </label>
+              <label class="btn">
+                <input type="radio" name="options" id="option4" value="4" />
+                <label for="option4">
+                  <span class=" btn">A bit better!</span>
+                </label>
+              </label>
+              <label class="btn">
+                <input type="radio" name="options" id="option5" value="5" />
+                <label for="option5">
+                  <span class=" btn">Way better!</span>
+                </label>
+              </label>
+            </div>
             <br/>
             <input type="submit" name="go" value="Submit" />
           </form>
         </div>
-        <div className="life" className="contents">
-          <h3 className="title-font">Your Life In Review</h3>
+        <div class="life" class="contents">
+          <h3 class="title-font">Your Life In Review</h3>
           <span>View all the great things that have happened to you!</span>
-          <hr />
+          <hr/>
         </div>
-        <div className="footer"></div>
-      </div> 
+        <div class="footer"><a href="#" class="logout">Logout</a></div>
+      </div>
     );
   }
 }
--- react/myapp/src/containers/home/Home.jsx
+++ react/myapp/src/containers/home/Home.jsx
@@ -33,10 +33,11 @@ class Home extends Component {
   }
 
   render() {
-    return (
+    // Get the fonts working here
+    return (      
       <div>
-        <ul id="bar">
-          <li><img className="logo" src="https://mticenter.com/mtopm/wp-content/uploads/2017/05/happy-face.png" alt="logo"/></li>
+        <ul className="bar">
+          <li><img className="logo" src={require('../Photos/NewLogo.png')} alt="logo"/></li>
           <li><a href="#home">Home</a></li>
           <li><a href="#features">Features</a></li>
           <li><a href="#about">About</a></li>
@@ -44,18 +45,29 @@ class Home extends Component {
         </ul>
 
         <div className="main">
-          <div id="home">
-            <img src="https://mticenter.com/mtopm/wp-content/uploads/2017/05/happy-face.png" alt="Logo" width="100%" />
-          </div>
+            <div className="home">
+                <p className="welcome">Welcome to iSteem!</p>
+                <button className="btn" type="button">Get Started</button>
+            </div>
 
-          <div id="features">
-            <p className="title">Features</p>
-            <p>content</p>
-          </div>
+            <div className="features">
+                <p className="title">Features</p>
+                <p>Relative Mood Ratings:<br/>These mood ratings are always relative to how you were feeling the last time you checked in. This way, you can gain a better understanding of your moods over time.<br/>
+                    <br/>Accomplishments:<br/>Put in any accomplishment or small thing that went well everyday! These will show up afterwards so you can have a nice trip down memory lane.
+                </p>
+            </div>
 
-          <div id="about">
-            <p className="title">About Us</p>
-          </div>
+            <div className="about">
+                <p className="title">About Us</p>
+                <p>iSteem is a platform that specficially does not allow for sharing of content. Everything that you put on here, is only for you to see.<br/><br/>
+                    The biggest problem facing us today is that everything is always in comparison to others. Something great happened in your life,
+                    but you look on social media and it seems that someone else has done something even better.
+                    This perpetual cycle of comparison is what leads to poor self-esteem, and a lack of worth.<br/><br/>
+                    We want to change that. Why not have a platform where you can just post things for yourself?
+                    Record what awesome things have happened in your life, without ever having to worry about what others will think.<br/><br/>
+                    We hope that through iSteem, you'll be able to learn to see things from a greater perspective,
+                    and appreciate all the amazing things that you do everyday in your life. </p>
+            </div>
         </div>
       </div>
     );
